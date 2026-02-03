35175100efbe78a8949564ed58975d117fc5eb24
dsi-brijesh <brijesh@demergsystems.com>
2026-02-03 15:42:09 +0530

    Update IDEA configuration and add `HomeScreen` with `TopAppBar` in `MainActivity.kt`

--- app/src/main/java/com/dsi/myapplication/MainActivity.kt
+++ app/src/main/java/com/dsi/myapplication/MainActivity.kt
@@ -4,12 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -27,7 +37,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme(darkTheme = false) {
                 Scaffold(
-                    modifier = Modifier.fillMaxSize().padding(16.dp),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
                 ) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -47,7 +59,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
@@ -60,4 +71,30 @@ fun GreetingPreview() {
             Greeting("Android")
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun HomeScreen() {
+    TopAppBar(
+        title = {
+            Text(text = "Home")
+        },
+        actions = {
+            Icon(
+                imageVector = Icons.Filled.Notifications,
+                contentDescription = "Notifications",
+                modifier = Modifier
+                    .padding(end = 12.dp)
+                    .clickable(
+                        onClick = {
+                            // show snacbar
+
+
+                        }
+                    )
+            )
+        }
+    )
 }

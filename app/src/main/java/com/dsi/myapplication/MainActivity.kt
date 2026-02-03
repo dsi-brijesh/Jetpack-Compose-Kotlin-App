61cce3ef62dbbd0440fc4ee549c1c2787973d4b5
Brijesh Rane <brijesh@demergsystems.com>
2026-02-03 16:28:06 +0530

    Refactor `MainActivity` and add `AppBar` with `Snackbar`
    Refactors `MainActivity.kt` to include a `Scaffold` with a `TopAppBar` and a `SnackbarHost`. The new `AppBar` composable displays a "Home" title and a clickable notification icon that triggers a `Snackbar`.
    
    Additionally, this commit adds new project code style configurations for XML and Kotlin and updates Android Studio settings for App Insights and deployment targets.

--- app/src/main/java/com/dsi/myapplication/MainActivity.kt
+++ app/src/main/java/com/dsi/myapplication/MainActivity.kt
@@ -15,6 +15,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -22,6 +24,8 @@ import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
@@ -29,6 +33,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.dsi.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,39 +42,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme(darkTheme = false) {
+                val snackBarHostState: SnackbarHostState = remember { SnackbarHostState() }
+                val scope: CoroutineScope = rememberCoroutineScope()
+
                 Scaffold(
+                    topBar = {
+                        AppBar(
+                            onShowSnackBar = {
+                                message -> scope.launch {
+                                    snackBarHostState.showSnackbar(message)
+                                }
+                            }
+                        )
+                    },
+                    snackbarHost = { SnackbarHost(snackBarHostState) },
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(16.dp),
                 ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    // Main screen content goes here
+                    Surface(modifier = Modifier.padding(innerPadding)) {
+                        Text("Content goes here")
+                    }
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Surface(
-            modifier = Modifier.padding(
-                vertical = 32.dp,
-                horizontal = 16.dp
-            )
-        ) {
-            Greeting("Android")
         }
     }
 }
@@ -76,7 +73,7 @@ fun GreetingPreview() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun HomeScreen() {
+fun AppBar(onShowSnackBar: (String) -> Unit = {}) {
     TopAppBar(
         title = {
             Text(text = "Home")
@@ -87,13 +84,9 @@ fun HomeScreen() {
                 contentDescription = "Notifications",
                 modifier = Modifier
                     .padding(end = 12.dp)
-                    .clickable(
-                        onClick = {
-                            // show snacbar
-
-
-                        }
-                    )
+                    .clickable {
+                        onShowSnackBar("Notification clicked!")
+                    }
             )
         }
     )
